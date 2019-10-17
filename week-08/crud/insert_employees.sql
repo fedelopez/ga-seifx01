fba09ba07495a1f6c6862afda53991676499f582
fedelopez <fede.lopez@gmail.com>
2019-10-17 13:31:46 +1100

    Add missing files from week 08

---
+++ week-08/crud/insert_employees.sql
@@ -0,0 +1,15 @@
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    business_title TEXT,
+    salary REAL
+);
+INSERT INTO employee (id, first_name, business_title, salary) VALUES ( 1, "Bill", "CTO", 195000.00 );
+INSERT INTO employee (id, first_name, business_title, salary) VALUES ( 2, "John", "Software Engineer", 180000.00 );
+INSERT INTO employee (id, first_name, business_title, salary) VALUES ( 3, "Jane", "Junior Software Engineer", 90000.80 );
+INSERT INTO employee (id, first_name, business_title, salary) VALUES ( 4, "Eddie", "DBA", 140000.80 );
+INSERT INTO employee (id, first_name, business_title, salary) VALUES ( 5, "Kate", "CEO", 200000.40 );
+INSERT INTO employee (id, first_name, business_title, salary) VALUES ( 6, "Bill", "Principal Software Engineer", 190000.80 );
+INSERT INTO employee (id, first_name, business_title, salary) VALUES ( 7, "Cecily", "Designer", 140000.80 );
+INSERT INTO employee (id, first_name, business_title, salary) VALUES ( 8, "Will", "Product Manager", 160000.20 );
+INSERT INTO employee (id, first_name, business_title, salary) VALUES ( 9, "James", null, 160000.20 );
